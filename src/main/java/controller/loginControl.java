21f686a9422ddc180ab4ab9899ba84775ac50e47
hycx520 <1137668244@qq.com>
2019-06-14 00:18:08 +0800

    1

--- src/main/java/controller/loginControl.java
+++ src/main/java/controller/loginControl.java
@@ -2,6 +2,6 @@ package controller;
 
 public class loginControl {
 	public static void main(String[] args) {
-		System.out.println("====+++++++++======");
+		System.out.println("====+++asd++++++======");
 	}
 }
