2d48d7830d3462c38310dd75a5ce60df4d69e469
hycx520 <1137668244@qq.com>
2019-06-18 17:08:11 +0800

    1

--- src/main/java/controller/loginControl.java
+++ src/main/java/controller/loginControl.java
@@ -1,7 +1,29 @@
 package controller;
 
-public class loginControl {
-	public static void main(String[] args) {
-		System.out.println("====+++asd++++++======");
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class loginControl extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("doGet");
+		super.doGet(req, resp);
 	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("doPost");
+		super.doPost(req, resp);
+	}
+	
+
+	
+	
 }
