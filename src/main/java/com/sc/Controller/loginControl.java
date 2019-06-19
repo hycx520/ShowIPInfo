7b0cfe2a9805147d8bb6e1a34d7ea935528f69f9
hycx520 <1137668244@qq.com>
2019-06-19 14:53:56 +0800

    1

---
+++ src/main/java/com/sc/Controller/loginControl.java
@@ -0,0 +1,10 @@
+package com.sc.Controller;
+
+import javax.servlet.http.HttpServlet;
+
+public class loginControl extends HttpServlet{
+
+	
+	
+	
+}
---
+++ src/main/java/com/sc/JDBC/JDBCConnect.java
@@ -0,0 +1,10 @@
+package com.sc.JDBC;
+
+public class JDBCConnect {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
